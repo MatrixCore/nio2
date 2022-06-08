5954245df513c0f90ca30c35178b0a68c52c3652
Finn Behrens <me@kloenk.dev>
2022-06-08 15:23:11 +0200

    Initial Commit

---
+++ Nio/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Nio
+//
+//  Created by Finn Behrens on 08.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nio/NioApp.swift
@@ -0,0 +1,17 @@
+//
+//  NioApp.swift
+//  Nio
+//
+//  Created by Finn Behrens on 08.06.22.
+//
+
+import SwiftUI
+
+@main
+struct NioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
